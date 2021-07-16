a0722ffff4a2f413ecdf3ca82a29c1e52cbdfce9
agiesey <adam@foreup.com>
2021-07-16 09:07:58 -0400

    cleaned up stylesheets, added media queries

--- src/Main/MainView.js
+++ src/Main/MainView.js
@@ -1,6 +1,7 @@
 import Marionette from 'backbone.marionette';
 import template from './MainTemplate.html';
 import _ from 'underscore';
+import './Main.scss';
 
 const MainView = Marionette.View.extend({
     template: _.template(template),
--- src/StockQuote/StockQuoteView.js
+++ src/StockQuote/StockQuoteView.js
@@ -1,6 +1,7 @@
 import Marionette from 'backbone.marionette';
-import template from './StockCardTemplate.html';
+import template from './StockQuoteCard.html';
 import _ from 'underscore';
+import './StockQuoteCard.scss'
 
 export const StockQuoteView = Marionette.View.extend({
     className: "stock-card",
