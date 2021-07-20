c605cb54c6bc488d659d98811ff5ac4e5d3a5372
agiesey <adam@foreup.com>
2021-07-20 11:56:27 -0400

    added dynamic styles for arrow graph

--- src/StockQuote/StockQuoteView.js
+++ src/StockQuote/StockQuoteView.js
@@ -5,5 +5,28 @@ import './StockQuoteCard.scss'
 
 export const StockQuoteView = Marionette.View.extend({
     className: "stock-card",
-    template: _.template(template)
+    template: _.template(template),
+
+    initialize: function() {
+        _.bindAll(this, 'template')
+    },
+
+    formatMoney: function(value) {
+        if (isNaN(parseInt(value))) {
+            return value
+        }
+
+        return parseFloat(value).toFixed(2)
+    },
+
+    getArrowTopOffset: function(price, high, low) {
+        const topMinOffset = 5;
+        const barHeight = 95;
+        const range = high - low;
+        const valuePerPixel = barHeight / range;
+        const topOffset = (high - price) * valuePerPixel;
+
+        return `${topOffset + topMinOffset}px`;
+
+    }
 })
